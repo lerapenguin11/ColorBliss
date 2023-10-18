4d11976a4d3d859f4ddfd2fd42f330dcf20dcee0
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-10-18 17:17:14 +0500

    screen layout coloring

--- app/src/main/java/com/example/colorbliss/presentation/MainActivity.kt
+++ app/src/main/java/com/example/colorbliss/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.colorbliss
+package com.example.colorbliss.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/example/colorbliss/utilits/Const.kt
+++ app/src/main/java/com/example/colorbliss/utilits/Const.kt
@@ -1,5 +1,5 @@
 package com.example.colorbliss.utilits
 
-import com.example.colorbliss.MainActivity
+import com.example.colorbliss.presentation.MainActivity
 
 lateinit var APP_ACTIVITY : MainActivity
